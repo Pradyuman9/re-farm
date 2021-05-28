c7e4d6b529d433abd08e2ddb823ede35e7e8980c
preet <preet>
2021-05-28 17:41:32 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-
+import { Provider } from 'react-redux'
 
 
 
